d35d86178e3aec9336043ec23929d75e8c5f4b6e
StillLearning01 <86482241+StillLearning01@users.noreply.github.com>
2021-06-27 11:34:29 +0800

    Add files via upload

---
+++ Hello World!.py
@@ -0,0 +1,17 @@
+#C8763
+print("")
+print("We're no strangers to love")
+print("You know the rules and so do I")
+print("A full commitment's what I'm thinking of")
+print("You wouldn't get this from any other guy")
+print("")
+print("I just wanna tell you how I'm feeling")
+print("Gotta make you understand")
+print("")
+print("Never gonna give you up")
+print("Never gonna let you down")
+print("Never gonna run around and desert you")
+print("Never gonna make you cry")
+print("Never gonna say goodbye")
+print("Never gonna tell a lie and hurt you")
+
